d4af1e7d3021c1731ec4988f0ee8e5d4495a5c0b
Matthew Nelson <developer@matthewnelson.io>
2023-03-05 15:32:45 -0500

    Add `Hmac` implementations (#2)

---
+++ library/hmac/hmac-md5/src/commonTest/kotlin/org/kotlincrypto/macs/HmacMD5UnitTest.kt
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2023 Matthew Nelson
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+package org.kotlincrypto.macs
+
+import org.kotlincrypto.core.Mac
+import kotlin.test.Test
+
+open class HmacMD5UnitTest: MacUnitTest() {
+    override fun mac(key: ByteArray): Mac = HmacMD5(key)
+
+    final override val expectedResetSmallHash: String = "da238c228f9d07fb28774118547bc6f7"
+    final override val expectedResetMediumHash: String = "067ddb79789e7716dc1571d394bddf55"
+    final override val expectedResetLargeHash: String = "721f3a9c5d28202e42d9c104f197efb0"
+    final override val expectedUpdateSmallHash: String = "5b964b6691857e0c94307b8dc5a67bc3"
+    final override val expectedUpdateMediumHash: String = "c690a1db88cd97970dc08f5df7109ca0"
+
+    @Test
+    override fun givenMac_whenResetSmallKey_thenDoFinalReturnsExpected() {
+        super.givenMac_whenResetSmallKey_thenDoFinalReturnsExpected()
+    }
+
+    @Test
+    override fun givenMac_whenResetMediumKey_thenDoFinalReturnsExpected() {
+        super.givenMac_whenResetMediumKey_thenDoFinalReturnsExpected()
+    }
+
+    @Test
+    override fun givenMac_whenResetLargeKey_thenDoFinalReturnsExpected() {
+        super.givenMac_whenResetLargeKey_thenDoFinalReturnsExpected()
+    }
+
+    @Test
+    override fun givenMac_whenUpdatedSmall_thenDoFinalReturnsExpected() {
+        super.givenMac_whenUpdatedSmall_thenDoFinalReturnsExpected()
+    }
+
+    @Test
+    override fun givenMac_whenUpdatedMedium_thenDoFinalReturnsExpected() {
+        super.givenMac_whenUpdatedMedium_thenDoFinalReturnsExpected()
+    }
+
+    @Test
+    override fun givenMac_whenCopied_thenIsDifferentInstance() {
+        super.givenMac_whenCopied_thenIsDifferentInstance()
+    }
+
+    @Test
+    override fun givenMac_whenDoFinal_thenLengthMatchesOutput() {
+        super.givenMac_whenDoFinal_thenLengthMatchesOutput()
+    }
+}
