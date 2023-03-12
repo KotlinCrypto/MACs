aa2c240b7adbca4a9634682ff77d17578b9fb97c
Matthew Nelson <developer@matthewnelson.io>
2023-03-12 06:17:15 -0400

    Update to use new digest name

--- library/hmac/hmac-sha1/src/commonTest/kotlin/org/kotlincrypto/macs/HmacSHA1UnitTest.kt
+++ library/hmac/hmac-sha1/src/commonTest/kotlin/org/kotlincrypto/macs/HmacSHA1UnitTest.kt
@@ -28,37 +28,37 @@ open class HmacSHA1UnitTest: MacUnitTest() {
     final override val expectedUpdateMediumHash: String = "d816e1912ba2cddf758d27fef55354b2bbf29bb7"
 
     @Test
-    override fun givenMac_whenResetSmallKey_thenDoFinalReturnsExpected() {
+    final override fun givenMac_whenResetSmallKey_thenDoFinalReturnsExpected() {
         super.givenMac_whenResetSmallKey_thenDoFinalReturnsExpected()
     }
 
     @Test
-    override fun givenMac_whenResetMediumKey_thenDoFinalReturnsExpected() {
+    final override fun givenMac_whenResetMediumKey_thenDoFinalReturnsExpected() {
         super.givenMac_whenResetMediumKey_thenDoFinalReturnsExpected()
     }
 
     @Test
-    override fun givenMac_whenResetLargeKey_thenDoFinalReturnsExpected() {
+    final override fun givenMac_whenResetLargeKey_thenDoFinalReturnsExpected() {
         super.givenMac_whenResetLargeKey_thenDoFinalReturnsExpected()
     }
 
     @Test
-    override fun givenMac_whenUpdatedSmall_thenDoFinalReturnsExpected() {
+    final override fun givenMac_whenUpdatedSmall_thenDoFinalReturnsExpected() {
         super.givenMac_whenUpdatedSmall_thenDoFinalReturnsExpected()
     }
 
     @Test
-    override fun givenMac_whenUpdatedMedium_thenDoFinalReturnsExpected() {
+    final override fun givenMac_whenUpdatedMedium_thenDoFinalReturnsExpected() {
         super.givenMac_whenUpdatedMedium_thenDoFinalReturnsExpected()
     }
 
     @Test
-    override fun givenMac_whenCopied_thenIsDifferentInstance() {
+    final override fun givenMac_whenCopied_thenIsDifferentInstance() {
         super.givenMac_whenCopied_thenIsDifferentInstance()
     }
 
     @Test
-    override fun givenMac_whenDoFinal_thenLengthMatchesOutput() {
+    final override fun givenMac_whenDoFinal_thenLengthMatchesOutput() {
         super.givenMac_whenDoFinal_thenLengthMatchesOutput()
     }
 }
