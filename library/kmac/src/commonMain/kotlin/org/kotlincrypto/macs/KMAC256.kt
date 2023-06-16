50a7c05fcbca2d96360c1bda173547a4e20f4d16
Matthew Nelson <developer@matthewnelson.io>
2023-06-16 10:32:04 -0400

    Improve `Kmac.Engine` performance (#38)

--- library/kmac/src/commonMain/kotlin/org/kotlincrypto/macs/KMAC256.kt
+++ library/kmac/src/commonMain/kotlin/org/kotlincrypto/macs/KMAC256.kt
@@ -69,7 +69,14 @@ public class KMAC256: Kmac {
         key: ByteArray,
         S: ByteArray?,
         outputLength: Int,
-    ): super(key, S, BIT_STRENGTH_256, outputLength)
+    ): this(key, S, outputLength, xofMode = false)
+
+    private constructor(
+        key: ByteArray,
+        S: ByteArray?,
+        outputLength: Int,
+        xofMode: Boolean,
+    ): super(key, S, BIT_STRENGTH_256, outputLength, xofMode)
 
     private constructor(engine: Mac.Engine): super(engine)
 
@@ -92,6 +99,9 @@ public class KMAC256: Kmac {
          * */
         @JvmStatic
         @JvmOverloads
-        public fun xOf(key: ByteArray, S: ByteArray? = null): Xof<KMAC256> = KMACXof(KMAC256(key, S, 0))
+        public fun xOf(
+            key: ByteArray,
+            S: ByteArray? = null,
+        ): Xof<KMAC256> = KMACXof(KMAC256(key, S, 0, xofMode = true))
     }
 }
