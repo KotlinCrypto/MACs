7d203b711a381f83ec39795c2b1d5a5fbf047261
Matthew Nelson <developer@matthewnelson.io>
2024-12-19 09:33:52 -0500

    Add benchmarks (#53)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -29,6 +29,7 @@ if (CHECK_PUBLICATION != null) {
         include(":library:$name")
     }
 
+    include(":benchmarks")
     include(":bom")
     include(":tools:testing")
     include(":test-android")
